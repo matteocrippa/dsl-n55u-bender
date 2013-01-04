0dda657f23e1479e6c5eb73adea808181ff39baa
Matteo Crippa <matteo.crippa@gmail.com>
2013-01-04 13:46:45 +0100

    add funny func to auto select current link

--- release/src/router/www/js/sidebar.js
+++ release/src/router/www/js/sidebar.js
@@ -5,34 +5,34 @@ var menuContent = "";
 
 menuContent += "<li class='nav-header'>Device</li>";
 menuContent += "<li class='active'><a href='/b_index.asp'>Status</a></li>";
-menuContent += "<li><a href=''>Administration</a></li>";
+menuContent += "<li><a href='/b_admin.asp'>Administration</a></li>";
 menuContent += "<li><a href='/b_logs.asp'>Logs</a></li>";
 
 // Dsl menu
 menuContent += "<li class='nav-header'>Dsl</li>";
 
 // put here Internet Connection, DMZ, DDNS, NAT Passthrough
-menuContent += "<li><a href=''>Dsl Settings</a></li>";
-menuContent += "<li><a href=''>Port Trigger</a></li>";
-menuContent += "<li><a href=''>Port Forwarding</a></li>";
+menuContent += "<li><a href='/b_dsl_settins.asp'>Dsl Settings</a></li>";
+menuContent += "<li><a href='/b_dsl_porttrigger.asp'>Port Trigger</a></li>";
+menuContent += "<li><a href='/b_dsl_portforwarding.asp'>Port Forwarding</a></li>";
 
 
 // Lan menu
 menuContent += "<li class='nav-header'>Lan</li>";
 
 // put here LAN IP, DHCP Server, IPTV
-menuContent += "<li><a href=''>IP Settings</a></li>";
-menuContent += "<li><a href=''>Route</a></li>";
+menuContent += "<li><a href='/b_lan_ip.asp'>IP Settings</a></li>";
+menuContent += "<li><a href='/b_lan_route.asp'>Route</a></li>";
 
 
 // Wireless menu
-menuContent += "<li class='nav-header'>Wireless</li>";
+menuContent += "<li class='nav-header'>WiFi</li>";
 
 // put here General 2.4GHz, Wireless MAC Filter 2.4GHz, Radius Setting 5GHz, Professional 2.4GHz
-menuContent += "<li><a href=''>2.4 GHz Settings</a></li>";
+menuContent += "<li><a href='/b_wifi_24.asp'>2.4 GHz Settings</a></li>";
 // put here General 5GHz, Wireless MAC Filter 5GHz, Radius Setting 5GHz, Professional 5GHz
-menuContent += "<li><a href=''>5 GHz Settings</a></li>";
-menuContent += "<li><a href=''>WPS</a></li>";
+menuContent += "<li><a href='/b_wifi_5.asp'>5 GHz Settings</a></li>";
+menuContent += "<li><a href='/b_wifi_wps.asp'>WPS</a></li>";
 
 
 // IPv6 menu
@@ -76,5 +76,17 @@ menuContent += "<li><a href=''>Traffic Manager</a></li>";
 menuContent += "<li><a href=''>Parental Control</a></li>";
 
 function showSidebar(){
+    // populate sidebar
     $('#sidebarmenu').html(menuContent);
+
+    // auto set current page with class='active'
+    $( "#sidebarmenu li" ).each(function( index ) {
+
+        // if this li contains a link
+        if($(this).find('a')[0]){
+            // check if it is current link in nav bar
+            if(location.href.search($(this).find('a')[0]) != -1)
+                $(this).addClass('active');
+        }
+    });
 }
